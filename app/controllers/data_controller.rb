c2b65152f6e0f0f68936ac19109adccc804dc3e0
Jonathan Harford <jonathan.harford@gmail.com>
2013-10-17 23:41:50 -0400

    Changed pull method from wget to Ruby's open-uri

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,19 +1,27 @@
 require 'csv'
+require 'open-uri'
+
 
 class DataController < ApplicationController
   def seed
-	`wget https://dl.dropboxusercontent.com/u/418477/reps.txt`	
-    `wget https://dl.dropboxusercontent.com/u/418477/Telephone_Directory.csv`
-	CSV.foreach('Telephone_Directory.csv') do |row|
-	   localartist = Artist.create(name:    (row[0].nil? ? "":row[0]) + " " + (row[1].nil? ? "":row[1]),
+	work_representations = open("http://dl.dropboxusercontent.com/u/418477/reps.txt").lines
+	dir_str = open("http://dl.dropboxusercontent.com/u/418477/Telephone_Directory.csv").read
+    CSV.parse(dir_str) do |row|
+		puts row[0]
+		puts row[1]
+		puts row[2]
+		puts row[3]
+       localartist = Artist.create(name:    (row[0].nil? ? "":row[0]) + " " + (row[1].nil? ? "":row[1]),
 	                               contact: (row[10].nil? ? "":row[10]),
 	                               bio:     (row[9].nil? ? "":row[9]), # We don't actually have a bio field
 	                               url:     (row[11].nil? ? "":row[11]),
 	                               location:(row[7].nil? ? "":row[7]) + " " + (row[6].nil? ? "":row[6]) + " " + (row[5].nil? ? "":row[5]))
-	               
+
 	   parentWork = Work.find_by_orig_id(row[4].nil? ? nil : row[4].rjust(4, '0'))
-	               
-	   localWork = Work.create(title:         (row[9].nil? ? "":row[9]) + " by " + (row[0].nil? ? "":row[0]) + " " + (row[1].nil? ? "":row[1]), # We don't actually have a title field
+
+	   localWork = Work.create(
+                   # We don't actually have a title field
+                   title:          (row[9].nil? ? "":row[9]) + " by " + (row[0].nil? ? "":row[0]) + " " + (row[1].nil? ? "":row[1]), 
 	               orig_id:        (row[3].nil? ? nil : row[3].rjust(4, '0')),
 	               orig_parent_id: (row[4].nil? ? nil : row[4].rjust(4, '0')),
 	               full_orig_id:   (row[3].nil? ? "" : row[3].rjust(4, '0')) + "_" + (row[4].nil? ? "" : row[4].rjust(4, '0')),
@@ -22,8 +30,7 @@ class DataController < ApplicationController
 	               artist_id:      localartist.id)
 	
 		
-		IO.foreach('reps.txt') do |f| 
-		    filename = File.basename(f)
+		work_representations.each do |filename| 
 		    if filename.start_with?(localWork.full_orig_id)
 		        WorkRepresentation.create(
 		            work_id: localWork.id,
