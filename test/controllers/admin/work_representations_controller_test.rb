24300edbca5025e93f9e708cff467c513525e94f
Daniel Talsky <danieltalsky@gmail.com>
2014-07-17 19:16:46 -0400

    created admin scaffolding, moved seed script

---
+++ test/controllers/admin/work_representations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::WorkRepresentationsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/admin/work_representations_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Admin::WorkRepresentationsHelperTest < ActionView::TestCase
+end
