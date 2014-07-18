cfe8218799b85b048937081cee789338b2727e5e
Daniel Talsky <danieltalsky@gmail.com>
2014-07-18 16:49:45 -0400

    removed admin controls from public view

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,41 +11,6 @@ class ArtistsController < ApplicationController
   def show
   end
 
-  # GET /artists/new
-  def new
-    @artist = Artist.new
-  end
-
-  # GET /artists/1/edit
-  def edit
-  end
-
-  # POST /artists
-  def create
-    @artist = Artist.new(artist_params)
-
-    if @artist.save
-      redirect_to @artist, notice: 'Artist was successfully created.'
-    else
-      render action: 'new'
-    end
-  end
-
-  # PATCH/PUT /artists/1
-  def update
-    if @artist.update(artist_params)
-      redirect_to @artist, notice: 'Artist was successfully updated.'
-    else
-      render action: 'edit'
-    end
-  end
-
-  # DELETE /artists/1
-  def destroy
-    @artist.destroy
-    redirect_to artists_url, notice: 'Artist was successfully destroyed.'
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_artist
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -36,41 +36,7 @@ class WorksController < ApplicationController
   def show
     @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)  
   end
-  
-  # GET /works/new
-  def new
-    @work = Work.new
-  end
 
-  # GET /works/1/edit
-  def edit
-  end
-
-  # POST /works
-  def create
-    @work = Work.new(work_params)
-
-    if @work.save
-      redirect_to @work, notice: 'Work was successfully created.'
-    else
-      render action: 'new'
-    end
-  end
-
-  # PATCH/PUT /works/1
-  def update
-    if @work.update(work_params)
-      redirect_to @work, notice: 'Work was successfully updated.'
-    else
-      render action: 'edit'
-    end
-  end
-
-  # DELETE /works/1
-  def destroy
-    @work.destroy
-    redirect_to works_url, notice: 'Work was successfully destroyed.'
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
