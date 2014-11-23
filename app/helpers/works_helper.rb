254b6dbba4cf8498a6536b960743dbc03e96f148
Daniel Talsky <danieltalsky@gmail.com>
2014-11-23 13:12:20 -0500

    Switched to resized works.

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -2,9 +2,9 @@ module WorksHelper
   def get_resized_jpg_path(fullsize_path)
 
     replacer_array = {
-      "/workrepresentations/" => "/workrepresentations/r/",
-      ".jpg" => "_w600.jpg",
-      ".png" => "_w600.jpg"
+      "/workrepresentations/" => "/workrepresentations/540/",
+      ".jpg" => "_540.jpg",
+      ".png" => "_540.png"
     }
 
     replacer_array.each do |find, replace|
