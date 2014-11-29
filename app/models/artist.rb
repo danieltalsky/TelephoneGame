a7fd73f0f5cba7e5c1bdb05b82b62b2a2cec46fc
Daniel Talsky <danieltalsky@gmail.com>
2014-11-28 22:01:25 -0500

    Smaller text size for long artist names
    fixes #8

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,14 @@
 class Artist < ActiveRecord::Base
   has_many :works
+  
+  def longname?
+    nameList = self.name.split(" ")
+    longname = false
+    nameList.each do |nameToken|
+      if nameToken.length > 9
+        longname = true
+      end
+    end
+    return longname
+  end
 end
