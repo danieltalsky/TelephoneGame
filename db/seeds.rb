a57c8c546463078c16a456f94cc9bc80a4c692cb
Daniel Talsky <danieltalsky@gmail.com>
2015-04-06 21:40:47 -0400

    Fix Daniel P misspelling once and for all

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,31 +27,31 @@ WorkRepresentation.create(
 )
 
 # Placeholder Curated Tour
-danielPadmosCuratedTour = CuratedTour.create(
-  tour_author: 'Daniel Padmos',
+danielPadnosCuratedTour = CuratedTour.create(
+  tour_author: 'Daniel Padnos',
   tour_name: 'A Curated Tour'
 )
-CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadmosCuratedTour.id, 
+CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadnosCuratedTour.id, 
   caption_text: 'Original Message: Oh god thy sea is so great and my boat is so small.')
-CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadmosCuratedTour.id, 
+CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadnosCuratedTour.id, 
   caption_text: 'The first recipient of the original message works through her resistance to “ideas of godhood”, finding comfort in “the idea of an expanding cosmos”. Her naked subject launches a folded paper boat onto the sea.')
-CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadmosCuratedTour.id, 
+CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadnosCuratedTour.id, 
   caption_text: 'After viewing Weaver’s painting, poet Bob Holman writes Naked Night, telling of  “…the poem that floats its message across the land that recedes to the stars themselves…” ')
-CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadmosCuratedTour.id, 
+CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadnosCuratedTour.id, 
   caption_text: 'In his musical composition for the Telephone, David Williams sustains a synthesized dominant C-chord throughout the 10 minute piece. Over this, a steely electric guitar twinkles as it picks out a repetitive G-A-B-G theme that never resolves, and as the piece goes on, sounds recorded in the deserts of Utah are perhaps carrying messages back to shore.')
-CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadmosCuratedTour.id, 
+CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadnosCuratedTour.id, 
   caption_text: 'From Williams’ music, Portland photographer Tom Patterson captures a decayed and abandoned pumping station and follows its pipeline to his final photo: a marvelous, natural, multi-tiered waterfall.')
-CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadmosCuratedTour.id, 
+CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadnosCuratedTour.id, 
   caption_text: 'Britain’s Claire Burke viewed Patterson’s photos, and then wrote her poem “The Flow of All Things”. As you continue through the line you may discover that it serves as a genuine game-changer!')
-CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadmosCuratedTour.id, 
+CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadnosCuratedTour.id, 
   caption_text: 'East village pub Jimmy’s No. 43 hosts a bimonthly reading series called Hearth Gods. Part of Hearth Gods’ 43rd session included Michael Rau’s direction and projection design with Sarah Mollo’s dramatic reading of Claire Burke’s poem. Mollo’s voice and body together with the brass instrumental accompaniment could provide a compelling eulogy at a New Orleans street funeral – or a New York East Village experience.')
-CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadmosCuratedTour.id, 
+CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadnosCuratedTour.id, 
   caption_text: 'From New York Burke’s poem was sent to Buenos Aires, where Sole Majdalani transformed it into an installation made from blocks of resin with drawings, origami and lights in a glass box. To fully appreciate it, watch the film she made, which reveals her work together with the poem, bit by bit.')
-CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadmosCuratedTour.id, 
+CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadnosCuratedTour.id, 
   caption_text: 'For Jana Weaver, the message flowed from the ocean to the cosmos; for Holman over land to the stars. In William’s music, the deserts of Utah and Patterson’s photos, as water through a mountain. Then Burke wrote “The Flow of All Things” about the loss of a loved one, amplified in Rau’s arousing production and then idealized in Majdalani’s elaborate sculpture. Now Chicago’s Kayce Bayer gives us his animation: “Afterlife” and the message moves on.')
-CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadmosCuratedTour.id, 
+CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadnosCuratedTour.id, 
   caption_text: 'Back in the UK, Emanuela Marenz and 11 others described their ideas of heaven. Visualizing each as a photographic subject, she produced this piece, called “Heavens”.')
-CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadmosCuratedTour.id, 
+CuratedTourStop.create(work_id: scWork.id, curated_tour_id: danielPadnosCuratedTour.id, 
   caption_text: 'Roberta Orlando of Italy did not know the title of Marenz’s photographic collage. In fact, none of the artists were privy to the titles of the works that inspired them. Even so, her translation of the message given to her was this surprising photograph, taken in Spain, which she calls “High Set”. A full year after Jana Weaver received the sailor’s prayer, nine works of art were inspired, one after the other, traversing the United States, the Western Hemisphere and the Atlantic ocean (twice), and still reminding us of our diminutive presence beside the greatness of nature.')  
   
   
