bf269069f03e49a5f156650f6285382899ef57bd
Mike Rogers <me@mikerogers.io>
2020-10-10 20:30:31 +0100

    Removing tests - none of them do anything it appears

--- test/helpers/work_representations_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class WorkRepresentationsHelperTest < ActionView::TestCase
-end
--- test/models/work_representation_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class WorkRepresentationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
